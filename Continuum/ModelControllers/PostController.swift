b5a587e8e12bce2e6b4fbf05e0fead71969fc342
Nick Reichard <reich452@regis.edu>
2018-09-20 11:17:26 -0600

    Fixed param and updated updated captionText argument

--- Continuum/ModelControllers/PostController.swift
+++ Continuum/ModelControllers/PostController.swift
@@ -23,12 +23,9 @@ class PostController{
         post.comments.append(comment)
     }
     
-    func createPostWith(image: UIImage, captionText: String, completion: @escaping (Post) -> ()){
-//        let post = Post(photo: image, captionText: captionText)
-//        
-//      
-//        publicDB.save(<#T##record: CKRecord##CKRecord#>, completionHandler: <#T##(CKRecord?, Error?) -> Void#>)
-//        comment.post = post
-//        self.posts.append(post)
+    func createPostWith(captionText: String, photo: UIImage, completion: @escaping (Post) -> ()){
+        let post = Post(caption: captionText, photo: photo)
+
+        self.posts.append(post)
     }
 }
