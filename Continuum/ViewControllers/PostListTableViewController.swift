6450af91d62ca2f980692a528b4fdb19424bb362
Nick Reichard <reich452@regis.edu>
2018-09-26 16:08:17 -0600

    Updated Operation

--- Continuum/ViewControllers/PostListTableViewController.swift
+++ Continuum/ViewControllers/PostListTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class PostListTableViewController: UITableViewController, UISearchBarDelegate {
     
@@ -16,17 +17,30 @@ class PostListTableViewController: UITableViewController, UISearchBarDelegate {
     
     var isSearching: Bool = false
     
+    lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .whiteLarge)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(spinner)
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            ])
+        return spinner
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         postSearchBar.delegate = self
-        tableView.prefetchDataSource = self 
+//        tableView.prefetchDataSource = self 
         
         let nc = NotificationCenter.default
         nc.addObserver(self, selector: #selector(postsChanged(_:)), name: PostController.PostsChangedNotification, object: nil)
         
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 350
-        fetchAllPosts()
+//        fetchAllPosts()
+        loadPhotos()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +52,53 @@ class PostListTableViewController: UITableViewController, UISearchBarDelegate {
     @objc func postsChanged(_ notification: Notification) {
         tableView.reloadData()
     }
+    
+    var queriedPosts = [Post]()
+    var postRecords: [CKRecord] = []
+    private func loadPhotos(cursor:  CKQueryOperation.Cursor? = nil) {
+        DispatchQueue.main.async {
+            self.spinner.startAnimating()
+        }
+        let operation: CKQueryOperation
+        
+        if let cursor = cursor {
+            operation = CKQueryOperation(cursor: cursor)
+        } else {
+            let predicate = NSPredicate(value: true)
+            let query = CKQuery(recordType: "Post", predicate: predicate)
+            query.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+            operation = CKQueryOperation(query: query)
+        }
+        operation.desiredKeys = ["caption", "photoData", "timestamp"]
+        operation.resultsLimit = 5
+        
+        operation.recordFetchedBlock = { [unowned self] record in
+            guard let post = Post(record: record) else { return }
+            self.queriedPosts.append(post)
+            print(self.queriedPosts.count)
+            DispatchQueue.main.sync {
+                let indexPath = IndexPath(item: self.queriedPosts.count - 1, section: 0)
+                self.tableView.insertRows(at: [indexPath], with: .fade)
+            }
+        }
+        let publicDB = CKContainer.default().publicCloudDatabase
+        operation.queryCompletionBlock = { [unowned self] cursor, error in
+            if let error = error {
+                print("Error fethcing posts \(error)")
+            } else if let cursor = cursor {
+                self.loadPhotos(cursor: cursor)
+                print("Fetching more results \(self.queriedPosts.count)")
+            } else {
+                print("Done")
+                DispatchQueue.main.async {
+                    self.spinner.stopAnimating()
+                }
+            }
+        }
+        publicDB.add(operation)
+        
+    }
+    
     
     func fetchAllPosts() {
         PostController.shared.fetchQueriedPosts { (firstPosts, continuedPosts) in
@@ -61,14 +122,14 @@ class PostListTableViewController: UITableViewController, UISearchBarDelegate {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return isSearching ? resultsArray?.count ?? 0: PostController.shared.posts.count
+        return isSearching ? resultsArray?.count ?? 0: queriedPosts.count
         
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as? PostTableViewCell
 //        let post = resultsArray?[indexPath.row] as? Post
-        let dataSource = isSearching ? resultsArray : PostController.shared.posts
+        let dataSource = isSearching ? resultsArray : queriedPosts
         let post = dataSource?[indexPath.row]
         cell?.post = post as? Post
         return cell ?? UITableViewCell()
@@ -103,7 +164,7 @@ class PostListTableViewController: UITableViewController, UISearchBarDelegate {
         if segue.identifier == "toPostDetailVC"{
             let destinationVC = segue.destination as? PostDetailTableViewController
             guard let indexPath = tableView.indexPathForSelectedRow else {return}
-            let post = PostController.shared.posts[indexPath.row]
+            let post = queriedPosts[indexPath.row]
             destinationVC?.post = post
         }
     }
@@ -114,7 +175,7 @@ extension PostListTableViewController: UITableViewDataSourcePrefetching {
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         
         for indexPath in indexPaths {
-            let post = PostController.shared.posts[indexPath.row]
+            let post = queriedPosts[indexPath.row]
             guard let url = post.imageAsset?.fileURL else {print("No tep url"); return }
             print("Prefetching \(post.caption)")
             URLSession.shared.dataTask(with: url).resume()
